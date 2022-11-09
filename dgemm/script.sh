cbee074b4b6cbb1a0b7922fece6ce094587086f2
Datky <iamsquel@gmail.com>
2022-11-09 12:35:22 +0100

    Added dgemm gcc performance files.
    Added performance files.

--- dgemm/script.sh
+++ dgemm/script.sh
@@ -2,12 +2,12 @@
 
 make
 
-taskset -c 2 ./dgemm 100 100 >> perf/dgemm_gcc_$1.dat
+taskset -c 2 ./dgemm 100 100 >> perf/$1/dgemm_$1_$2.dat
 
-sed -n "2p;" perf/dgemm_gcc_$1.dat >> perf/gcc/dgemm_gcc_$1_ijk.dat
-sed -n "3p;" perf/dgemm_gcc_$1.dat >> perf/gcc/dgemm_gcc_$1_ikj.dat
-sed -n "4p;" perf/dgemm_gcc_$1.dat >> perf/gcc/dgemm_gcc_$1_iex.dat
-sed -n "5p;" perf/dgemm_gcc_$1.dat >> perf/gcc/dgemm_gcc_$1_unroll.dat
-sed -n "6p;" perf/dgemm_gcc_$1.dat >> perf/gcc/dgemm_gcc_$1_cblas.dat
+sed -n "2p;" perf/$1/dgemm_$1_$2.dat >> perf/$1/dgemm_$1_$2_ijk.dat
+sed -n "3p;" perf/$1/dgemm_$1_$2.dat >> perf/$1/dgemm_$1_$2_ikj.dat
+sed -n "4p;" perf/$1/dgemm_$1_$2.dat >> perf/$1/dgemm_$1_$2_iex.dat
+sed -n "5p;" perf/$1/dgemm_$1_$2.dat >> perf/$1/dgemm_$1_$2_unroll.dat
+sed -n "6p;" perf/$1/dgemm_$1_$2.dat >> perf/$1/dgemm_$1_$2_cblas.dat
 
 rm dgemm
