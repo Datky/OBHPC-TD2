2bcf65f334ecff5b82c776ea53e2373def5ef59a
Datky <iamsquel@gmail.com>
2022-11-09 12:22:17 +0100

    Added bash script for automation.
    Added script.

---
+++ dgemm/script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+taskset -c 2 ./dgemm 100 100 >> perf/dgemm_gcc_$1.dat
+
+sed -n "2p;" perf/dgemm_gcc_$1.dat >> perf/gcc/dgemm_gcc_$1_ijk.dat
+sed -n "3p;" perf/dgemm_gcc_$1.dat >> perf/gcc/dgemm_gcc_$1_ikj.dat
+sed -n "4p;" perf/dgemm_gcc_$1.dat >> perf/gcc/dgemm_gcc_$1_iex.dat
+sed -n "5p;" perf/dgemm_gcc_$1.dat >> perf/gcc/dgemm_gcc_$1_unroll.dat
+sed -n "6p;" perf/dgemm_gcc_$1.dat >> perf/gcc/dgemm_gcc_$1_cblas.dat
+
+rm dgemm
